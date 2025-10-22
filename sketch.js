c242c72f337c0cfa64739caf3bfdb49c5b503687
deborah-orret <deborahorret@gmail.com>
2025-10-22 11:15:51 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+function login() {
+  if (document.getElementById("username").value == "bestsherriffintown" && document.getElementById("password").value == caesarCipher("bestsherriffintown", 3)) {
+	  window.location.href='dataresults.html'
+  } else {
+	  document.getElementById("wrongcredentials").innerHTML = "Username or password is incorrect. Please try again.";
+  }
+}
+
+
+function caesarCipher(word, shift) {
+var result = "";
+for (var i = 0; i < word.length; i++) {
+var char = word[i];
+if (char.match(/[a-z]/i)) {
+var code = word.charCodeAt(i);
+if (code >= 65 && code <= 90) {
+result += String.fromCharCode(((code - 65 + shift) % 26) + 65);
+}
+// Lowercase letters
+else if (code >= 97 && code <= 122) {
+result += String.fromCharCode(((code - 97 + shift) % 26) + 97);
+}
+} else {
+result += char;
+}
+}
+return result;
+}
